32aace312676ef21ad8be617ff59a395a9ee5cdc
Wes Karle <wesleykarle@gmail.com>
2019-08-19 23:58:38 -0700

    Add project files.

---
+++ Models/MyAsyncMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+
+        public async static Task<long?> GetPageLength()
+        {
+
+            HttpClient client = new HttpClient();
+
+            var httpMessage = await client.GetAsync("http://apress.com");
+
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
---
+++ Models/MyExtensionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+            decimal total = 0;
+            foreach (Product prod in products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+        }
+        public static IEnumerable<Product> Filter(
+            this IEnumerable<Product>productEnum, Func<Product, bool> selector)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if ((selector(prod)))
+                {
+                    yield return prod;
+                }
+            }
+        }
+        public static IEnumerable<Product> FilterByName(
+            this IEnumerable<Product> productEnum, char firstLetter)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (prod?.Name?[0] == firstLetter)
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+}
